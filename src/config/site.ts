89c9c413b509b8865e2bd4a962776c84aa380190
L200160067 <kurialfarez@gmail.com>
2026-08-18 12:49:58 +0700

    fix: update default WhatsApp number in site configuration

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig = {
     cdnUrl: process.env.NEXT_PUBLIC_CDN_URL || 'https://l200160067.github.io/mone-assets',
     logo: process.env.NEXT_PUBLIC_CDN_URL ? `${process.env.NEXT_PUBLIC_CDN_URL}/images/branding/mone.webp` : 'https://l200160067.github.io/mone-assets/images/branding/mone.webp',
     whatsapp: {
-        number: process.env.NEXT_PUBLIC_WHATSAPP_NUMBER || "6285168850712",
+        number: process.env.NEXT_PUBLIC_WHATSAPP_NUMBER || "6285879535070",
         defaultMessage: "Halo M-One Solution, saya ingin berkonsultasi mengenai layanan Anda."
     },
     social: {
